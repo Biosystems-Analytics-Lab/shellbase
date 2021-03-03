434fd283c7e47ed74109509056dd2876dfacae95
DANRAMAGE <dan@inlet.geol.sc.edu>
2021-03-03 11:37:21 -0500

    Swap out openpyxl for pandas to read the xlsx file.

--- code/import_examples/ga_xls2db.py
+++ code/import_examples/ga_xls2db.py
@@ -1,7 +1,6 @@
 '''
 Requirements:
-    openpyxl - Unfortunately the xlrd library doesn't handle xlsx files, but openpyxl does.
-
+    pandas
 Command line:
     python ga_xls2db.py --ExcelFile=<path to the datafile>
 
@@ -10,8 +9,9 @@ import sys
 import optparse
 import traceback
 from datetime import datetime
-from openpyxl import load_workbook
+import pandas as pd
 
+#import psycopg2
 
 class Sample:
     def __init__(self):
@@ -37,54 +37,55 @@ def parse_worksheet(xls_filename):
     samples = []
     try:
         #OPen up the excel file as a workbook.
-        workbook = load_workbook(filename=xls_filename)
+        #workbook = load_workbook(filename=xls_filename)
+        xl_file = pd.read_excel(xls_filename)
     except Exception as e:
         traceback.print_exc()
     else:
-        try:
+        #try:
             #For the time being, we just assume we are interested in the first worksheet.
-            worksheet = workbook.active
-        except Exception as e:
-            traceback.print_exc()
-        else:
-            for row_ndx, data_row in enumerate(worksheet.iter_rows()):
-                #row 0 is the header.
-                if row_ndx > 0:
-                    print("Processing row: %d" % (row_ndx))
-                    try:
-                        sample = Sample()
-                        sample.station_id = data_row[1].value
-                        parts = data_row[0].value.split('/')
-                        #Zero pad the date and create a datetime object.
-                        date = datetime.strptime('%02d/%02d/%d' % (int(parts[0]), int(parts[1]), int(parts[2])), "%m/%d/%Y")
-                        time = data_row[2].value
-                        #We combine the individual date and time columns into a python datetime object.
-                        sample.sample_datetime = datetime.combine(date, time)
-                        sample.latitude = data_row[3].value
-                        sample.longitude = data_row[4].value
-                        sample.sample_value = data_row[10].value
-                        sample.tube_code = data_row[11].value
-                        sample.tide_code = data_row[12].value
+            #worksheet = workbook.active
+        #except Exception as e:
+        #    traceback.print_exc()
+        #else:
+        for row_ndx, data_row in xl_file.iterrows():
+        #for row_ndx, data_row in enumerate(worksheet.iter_rows()):
+            print("Processing row: %d" % (row_ndx))
+            try:
+                sample = Sample()
+                sample.station_id = data_row['StationID']
+                parts = data_row['DateCollected'].split('/')
+                #Zero pad the date and create a datetime object.
+                date = datetime.strptime('%02d/%02d/%d' % (int(parts[0]), int(parts[1]), int(parts[2])), "%m/%d/%Y")
+                time = data_row['Time']
+                #We combine the individual date and time columns into a python datetime object.
+                sample.sample_datetime = datetime.combine(date, time)
+                sample.latitude = data_row['Latitude']
+                sample.longitude = data_row['Longitude']
+                sample.sample_value = data_row['FecalColiform 100/mL']
+                sample.tube_code = data_row['TubeCode']
+                sample.tide_code = data_row['TideCode']
 
-                        sample.temperature = data_row[5].value
-                        sample.dissolved_oxygen = data_row[6].value
-                        sample.conductivity = data_row[7].value
-                        sample.ph = data_row[8].value
-                        sample.salinity = data_row[9].value
+                sample.temperature = data_row['Temp °C']
+                sample.dissolved_oxygen = data_row['DO mg/L']
+                sample.conductivity = data_row['Conductivity mS/cm']
+                sample.ph = data_row['pH']
+                sample.salinity = data_row['Salinity ppt']
 
-                        samples.append(sample)
+                samples.append(sample)
 
-                    except Exception as e:
-                        print("Error on row: %d" % (row_ndx))
-                        traceback.print_exc()
-    return
+            except Exception as e:
+                print("Error on row: %d" % (row_ndx))
+                traceback.print_exc()
+    return samples
 
 def save_to_database(samples):
     return
 def main():
     parser = optparse.OptionParser()
-    parser.add_option("-c", "--ExcelFile", dest="xl_file", default=None,
+    parser.add_option("--ExcelFile", dest="xl_file", default=None,
                       help="Excel data file to import.")
+
 
     (options, args) = parser.parse_args()
 
